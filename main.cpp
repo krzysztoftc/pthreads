eb9ca8bed8168061e91fa692f13d9fc29f68f545
Krzysztof Cabała <krzysztoftc@p.pl>
2016-05-23 15:21:48 +0200

    first run

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <pthread.h>
 
 using namespace std;
 
+void *thread(void *arg){
+    cout<< "Hello, I want say you: " << endl;
+    return NULL;
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    pthread_t id;
+
+    pthread_create(&id, NULL, thread, NULL);
+
+    pthread_join(id , NULL);
+
+    cout<< "Main thread" << endl;
+
     return 0;
 }
